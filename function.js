f1c8c4f7dcc73b8f68d68693e8bd4a1918bd78d6
usaid7015 <49516724+usaid7015@users.noreply.github.com>
2019-07-18 16:06:34 +0500

    Add files via upload

---
+++ function.js
@@ -0,0 +1,34 @@
+//square ky lye is trha function bnyga
+
+function squareNumber(num1) {
+    return num1 * num1;
+}
+squareNumber(6);
+
+// cube k lye or is trha se or bhi nums multiply kr skty hain
+function cubeicNumber(num1) {
+    return num1 * num1 * num1;
+}
+squareNumber(3);
+//name k call py greatings show hoga.
+// function myName(name) {
+//     return "welcome, " + name + "!";
+// }
+
+// function greet(name){
+//     return "Congratulations," + name + "!" ;
+// }
+//second methood to write function nut call krny k lye ek hi tareeka hai.
+
+let greet = function (name) {
+    return "Congratulations," + name + "!";
+}
+
+function name(str){
+return str.length;
+}
+
+function myNam(string) {
+    return string.toUpperCase();
+     
+},
